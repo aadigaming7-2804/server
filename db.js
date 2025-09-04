5d178d65b896a2bf0f3dd6189512d663458836d3
aadigaming7-2804 <aadigaming.7@gmail.com>
2025-09-04 12:54:23 +0530

    db.js

---
+++ db.js
@@ -0,0 +1,10 @@
+// db.js
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/mydb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log(" MongoDB connected"))
+.catch(err => console.log(" MongoDB connection error:", err));
+
+module.exports = mongoose;
